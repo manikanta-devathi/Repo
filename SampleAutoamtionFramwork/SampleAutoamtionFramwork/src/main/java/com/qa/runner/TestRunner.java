3360c0b8cd5e87657d27a97e7a73f2bfa825d18b
manikanta-devathi <70749660+manikanta-devathi@users.noreply.github.com>
2020-09-04 14:43:43 +0530

    Add files via upload

---
+++ SampleAutoamtionFramwork/SampleAutoamtionFramwork/src/main/java/com/qa/runner/TestRunner.java
@@ -0,0 +1,7 @@
+package com.qa.runner;
+
+//This package we can use to create all runner classes related to particular feature
+
+public class TestRunner {
+
+}
---
+++ SampleAutoamtionFramwork/SampleAutoamtionFramwork/src/main/java/com/qa/utils/TestUtil.java
@@ -0,0 +1,10 @@
+package com.qa.utils;
+
+/* This package is used to setup all basic configuration steps. example calling property files, setting webdriver 
+connectivites, sql connectivities.
+We can create different utility files for different set of configurations 
+
+*/
+public class TestUtil {
+	
+}
